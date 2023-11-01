aa09bfb45abc4a7de4c8aa1f1c0208e3b68e8149
shivProgramer <shivapal6390@gmail.com>
2023-11-01 19:18:54 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Badge from '@mui/material/Badge';
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+    <Container>
+      <Navbar.Brand href="#home">Add to cart </Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+      </Nav>
+      <Badge badgeContent={4} color="primary">
+      <i class="fa-solid fa-cart-shopping text-light" style={{fontSize:25, cursor:'pointer'}}></i>
+    </Badge>
+      
+    </Container>
+  </Navbar>
+  )
+}
+
+export default Header
